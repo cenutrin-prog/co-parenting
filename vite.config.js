150c0ab3d8bffa4d103738d56ea995e6d0860878
cenutrin-prog <cenutrin@gmail.com>
2025-11-26 20:39:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    copyPublicDir: true
   }
 })
